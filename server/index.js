388d4519affeb212d2e852b5ad3ca1df36a4dd4b
Eric Normand <ericwnormand@gmail.com>
2017-05-03 03:28:44 -0500

    Wow, getting better!

--- server/index.js
+++ server/index.js
@@ -7,16 +7,18 @@ const PORT = process.env.PORT || 5000;
 
 const mustacheExpress = require('mustache-express');
 
+const debug = require('debug')('http');
+
 // Register '.html' extension with The Mustache Express
 app.engine('html', mustacheExpress());
 
 app.set('view engine', 'mustache');
-app.set('views', path.resolve(__dirname, '../react-ui/build'));
+app.set('views', path.resolve(__dirname, '../react-ui/views'));
 
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 
-const backend = 'http://www.bouncedotcom.com';
+const backend = 'https://bouncedotcom-backend.herokuapp.com';
 
 app.get('/twerker/:tid', function(request, response) {
   axios({
@@ -53,15 +55,17 @@ app.get('/twerker/:tid', function(request, response) {
 });
 
 app.get('/bounce/:bid', function(request, response) {
+  debug(`${backend}/bounces/${request.params.bid}`);
   axios({
     method: 'get',
-    url: `${backend}/users/${request.params.tid}`,
+    url: `${backend}/bounces/${request.params.bid}`,
   }).then((resp) => {
+    debug('here');
     const pid = resp.data.cloudinary_id;
     const pty = resp.data.media_type;
 
     var imageurl = "http://www.bouncedotcom.com/default.jpg";
-
+    
     if(pid && pty === 'image') {
       imageurl = `http://res.cloudinary.com/bouncedotcom-com/image/upload/${pid}.jpg`;
     }
@@ -70,21 +74,34 @@ app.get('/bounce/:bid', function(request, response) {
     }
     response.render('index.html', {
       image: imageurl,
-      description: "Twerkin' for the money!",
+      description: "hello", 
       title: "One fabulous twerker",
       type: "video.movie",
       url: `http://www.bouncedotcom.com/bounce/${request.params.bid}`
     });
   }).catch(err => {
+    debug(err);
     response.render('index.html', {
       image: "http://www.bouncedotcom.com/default.jpg", // TODO
-      description: "Twerkin' for the money!",
+      description: "yo",
       title: "One fabulous twerker",
       type: "video.movie",
       url: `http://www.bouncedotcom.com/bounce/${request.params.bid}`
     });
   });
 });
+
+app.get('/', function(request, response) {
+  response.render('index.html', {
+    image: "",
+    description: "Twerkin' for the money!",
+    title: "Bounce DOT COM .com",
+    type: "video.movie",
+    url: "http://www.bouncedotcom.com/"
+  });
+});
+
+
 // All remaining requests return the React app, so it can handle routing.
 app.get('*', function(request, response) {
   response.render('index.html', {
