ed60e17d2bc60e7c33862a376fb6066b29da2aa6
Eric Normand <ericwnormand@gmail.com>
2017-05-03 05:49:10 -0500

    Add facebook video tags.

--- server/index.js
+++ server/index.js
@@ -29,19 +29,23 @@ app.get('/twerker/:tid', function(request, response) {
     const pty = resp.data.payload && resp.data.payload.profile_pic_type;
 
     var imageurl = "http://www.bouncedotcom.com/default.jpg";
+    var video = null;
 
     if(pid && pty === 'image') {
       imageurl = `http://res.cloudinary.com/bouncedotcom-com/image/upload/${pid}.jpg`;
     }
     if(pid && pty === 'video') {
       imageurl = `http://res.cloudinary.com/bouncedotcom-com/video/upload/${pid}.jpg`;
+      video = `http://res.cloudinary.com/bouncedotcom-com/video/upload/${pid}.mp4`;
+
     }
     response.render('index.html', {
       image: imageurl,
       description: "Twerkin' for the money!",
       title: "One fabulous twerker",
       type: "video.movie",
-      url: `http://www.bouncedotcom.com/twerker/${request.params.tid}`
+      url: `http://www.bouncedotcom.com/twerker/${request.params.tid}`,
+      video: video
     });
   }).catch(err => {
     response.render('index.html', {
@@ -65,19 +69,23 @@ app.get('/bounce/:bid', function(request, response) {
     const pty = resp.data.media_type;
 
     var imageurl = "http://www.bouncedotcom.com/default.jpg";
+
+    var video = null;
     
     if(pid && pty === 'image') {
       imageurl = `http://res.cloudinary.com/bouncedotcom-com/image/upload/${pid}.jpg`;
     }
     if(pid && pty === 'video') {
       imageurl = `http://res.cloudinary.com/bouncedotcom-com/video/upload/${pid}.jpg`;
+      video = `http://res.cloudinary.com/bouncedotcom-com/video/upload/${pid}.mp4`;
     }
     response.render('index.html', {
       image: imageurl,
       description: "hello", 
       title: "One fabulous twerker",
       type: "video.movie",
-      url: `http://www.bouncedotcom.com/bounce/${request.params.bid}`
+      url: `http://www.bouncedotcom.com/bounce/${request.params.bid}`,
+      video: video
     });
   }).catch(err => {
     debug(err);
