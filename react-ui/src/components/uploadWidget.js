1c29b0577bbf894e640ca85541d1c918759e271f
Eric Normand <ericwnormand@gmail.com>
2017-05-05 09:53:48 -0500

    Too large.

--- react-ui/src/components/uploadWidget.js
+++ react-ui/src/components/uploadWidget.js
@@ -15,7 +15,7 @@ export default class UploadWidget extends Component {
       upload_preset: 'default',
       tags: ['bounce'],
       theme: 'purple',
-      max_file_size: 95000000
+      max_file_size: 35000000
     },
     cb);
   }
